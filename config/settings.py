e2da179b81a1d43f4c61386fe0934f5a8d3c32d4
DuckRainbow <sillestyna@gmail.com>
2024-04-29 23:12:03 +0500

    task 4

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,16 @@
+
+
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+SECRET_KEY = 'django-insecure-*-9c07g^vz&j-+_dl%*3vqwt#*!ij4^u1f=i%@9$ul#xx9_3ty'
+
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -47,12 +53,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -69,6 +78,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -77,8 +87,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (BASE_DIR / 'static',)
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
